fba6665987a4cc3639dc209dff3740cdd0fddbe1
bbeomgeun <miki30808@gmail.com>
2020-10-02 00:51:13 +0900

    BackTracking

--- baekjoon15655.cpp
+++ baekjoon15655.cpp
@@ -4,21 +4,17 @@
 using namespace std;
 
 int N, M, input, arr[8], result[8];
-bool check[10001];
 
 void backTracking(int depth, int next) {
 	if (M == depth) {
-		for (int i = 0; i < N; i++)
+		for (int i = 0; i < M; i++)
 			cout << result[i] << " ";
 		cout << "\n";
 		return;
 	}
 	for (int k = next; k < N; k++) {
-		if (!check[arr[k]]) {
-			check[arr[k]] = true;
-			result[depth] = arr[k];
-			backTracking(depth + 1, next + 1);
-		}
+		result[depth] = arr[k];
+		backTracking(depth + 1, k+1);
 	}
 }
 
@@ -28,7 +24,7 @@ int main() {
 		cin >> input;
 		arr[i] = input;
 	}
-	sort(arr, arr + 8);
+	sort(arr, arr + N);
 	backTracking(0, 0);
 	return 0;
 }
