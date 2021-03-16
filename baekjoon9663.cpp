d41451578bc71c9123464aae542be9672b5f8cac
bbeomgeun <miki308@naver.com>
2021-03-16 18:00:18 +0900

    N-Queen

--- baekjoon9663.cpp
+++ baekjoon9663.cpp
@@ -1,83 +1,30 @@
 #include <iostream>
+
 using namespace std;
 
-int Qarray[10001], N, input;
-string s;
+int n, answer, carr[16];
 
-class array_Queue {
-public:
-	int n;
-	int start;
-	int end;
+void queen(int y) { // y는 열을 뜻한다.
 
-	array_Queue() {
-		start = 0;
-		n = 0;
-		end = 0;
+	if (y == n ) { //정상적으로 n에 해당하면 정답
+		answer++;
+		return;
 	}
 
-	bool empty() {
-		return n == 0;
-	}
-	
-	void push(int X) {
-		Qarray[end] = X;
-		n++;
-		end++;
-	}
-
-	int pop() {
-		if (empty()) {
-			return -1;
+	for (int i = 0; i < n; i++) { 
+		carr[y] = i; // 해당 열의 행 정보에 1~n까지 넣어본다
+		int flag = true;
+		for (int j = 0; j < y; j++) { // 첫번째부터 y번째 열까지 비교해본다
+			if (carr[y] == carr[j] || abs(carr[y] - carr[j]) == y - j) // 예) 3번째열이면 1,2번째 열들의 정보와 비교한다. 1번째에 3열, 3번째에 3열이면 오답
+				flag = false;
 		}
-		else {
-			int temp = Qarray[start];
-			start++;
-			n--;
-			return temp;
-		}
+		if(flag) // 만약 이번 열 carr[y] = i의 행정보가 위의 열들과 안겹치면 다음 열로 간다.
+			queen(y + 1);
 	}
-
-	int front() {
-		if (empty())
-			return -1;
-		else
-			return Qarray[start];
-	}
-
-	int back() {
-		if (empty())
-			return -1;
-		else
-			return Qarray[end-1];
-	}
-
-	int size() {
-		return n;
-	}
-};
+}
 
 int main() {
-	cin >> N;
-	array_Queue aq;
-	for (int i = 0; i < N; i++) {
-		cin >> s;
-		if (s == "push") {
-			cin >> input;
-			aq.push(input);
-		}
-		else if (s == "front") 
-			cout << aq.front() << "\n";
-		else if (s == "back")
-			cout << aq.back() << "\n";
-		else if (s == "empty")
-			cout << aq.empty() << "\n";
-		else if (s == "size")
-			cout << aq.size() << "\n";
-		else if (s == "pop")
-			cout << aq.pop() << "\n";
-	}
-
-
-	return 0;
+	cin >> n;
+	queen(0);
+	cout << answer;
 }
