50253921d48b6312b7a33c013b685d26e5f0e7dc
bbeomgeun <miki308@naver.com>
2021-04-04 20:34:25 +0900

    DFS+DP

--- baekjoon1937.cpp
+++ baekjoon1937.cpp
@@ -2,25 +2,26 @@
 
 using namespace std;
 
-int n, arr[501][501], dp[501][501], check[501][501];
+int n, arr[501][501], dp[501][501];
 int xmove[4] = { 1, -1, 0, 0 }, ymove[4] = { 0,0,1,-1 };
 
 int DFS(int startx, int starty) {
-	if (check[startx][starty])
-		return 0;
+	if (dp[startx][starty] != -1)
+		return dp[startx][starty]+1;
+
 	else {
-		check[startx][starty] = true;
-		
+		dp[startx][starty] = 1;
+
 		for (int i = 0; i < 4; i++) {
 			int x = startx + xmove[i];
 			int y = starty + ymove[i];
 			if (x >= 0 && x < n && y >= 0 && y < n) {
-				if (!check[x][y] && arr[x][y] > arr[startx][starty]) {
-					dp[startx][starty] += DFS(x, y);
+				if (arr[x][y] > arr[startx][starty]) {
+					dp[startx][starty] = max(dp[startx][starty], DFS(x, y));
 				}
 			}
 		}
-		return 1;
+		return dp[startx][starty]+1;
 	}
 }
 
@@ -29,15 +30,16 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> arr[i][j];
+			dp[i][j] = -1;
 		}
 	}
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (!check[i][j])
 				DFS(i, j);
 		}
 	}
+
 	int mx = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
