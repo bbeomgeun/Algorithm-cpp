21be9ce29a396357669bc73ec3238132b3e0943d
bbeomgeun <miki308@naver.com>
2021-11-22 23:45:45 +0900

    G5 음식 평론가 [수학]

---
+++ baekjoon1188.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int gcd(int a, int b) {
+	if (b == 0) return a;
+	return gcd(b, a % b);
+}
+
+int main() {
+	cin >> n >> m;
+	cout << m - gcd(n, m);
+}
